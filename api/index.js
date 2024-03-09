3cbd1e588b23bfc3170416a005a342a00d1c381a
suneelcrestha9 <126167086+suneelcrestha9@users.noreply.github.com>
2024-03-09 21:21:55 +0545

    build change

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from "./src/routes/user.route.js";
 import userAuthRouter from "./src/routes/auth.route.js";
 import blogRouter from './src/routes/blog.route.js'
 import commentRouter from './src/routes/comment.router.js'
+import path from 'path'
 
 connetcDB()
 .then(()=>{
@@ -17,6 +18,7 @@ connetcDB()
     console.log("Errro",error)
 })
 
+const __dirname = path.resolve()
 //declaring api and creating post
 app.use('/api/v1',userRouter)
 app.use('/api/v1',blogRouter)
@@ -25,8 +27,10 @@ app.use('/api/v1',commentRouter)
 //api for singup , login
 app.use('/api/v1',userAuthRouter) //userAuthRouter is the define in the routes in auth.routes.js file
 
-
-
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 //handeling errors
 app.use((err, req, res, next) => {
